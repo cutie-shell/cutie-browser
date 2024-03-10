038453969996d473cc962f0aa38337ed91508686
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2024-03-10 14:54:40 +0200

    treewide: Unify formatting
    Use clang-format on all c++ files.

--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,42 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts, true);
-    
-    QtWebEngineQuick::initialize();
-    QGuiApplication app(argc, argv);
-    
-    QGuiApplication::setApplicationName("cutie-browser");
+	QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts, true);
 
-    QCommandLineParser parser;
-    parser.setApplicationDescription("The Browser for Cutie");
-    parser.addHelpOption();
+	QtWebEngineQuick::initialize();
+	QGuiApplication app(argc, argv);
 
-    QCommandLineOption webAppOption(QStringList() << "webappurl",
-            QCoreApplication::translate("main", "The url to load. If not empty, browser starts without address bar and navigation buttons."),
-            QCoreApplication::translate("main", "url"));
-    parser.addOption(webAppOption);
-    parser.process(app);
-    QString webAppUrl = parser.value(webAppOption);
+	QGuiApplication::setApplicationName("cutie-browser");
 
-    QQmlApplicationEngine engine;
+	QCommandLineParser parser;
+	parser.setApplicationDescription("The Browser for Cutie");
+	parser.addHelpOption();
 
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+	QCommandLineOption webAppOption(
+		QStringList() << "webappurl",
+		QCoreApplication::translate(
+			"main",
+			"The url to load. If not empty, browser starts without address bar and navigation buttons."),
+		QCoreApplication::translate("main", "url"));
+	parser.addOption(webAppOption);
+	parser.process(app);
+	QString webAppUrl = parser.value(webAppOption);
 
-    if(webAppUrl != "")
-        QQmlProperty(engine.rootObjects()[0], "webAppUrl").write(webAppUrl);
+	QQmlApplicationEngine engine;
 
-    return app.exec();
+	const QUrl url(QStringLiteral("qrc:/main.qml"));
+	QObject::connect(
+		&engine, &QQmlApplicationEngine::objectCreated, &app,
+		[url](QObject *obj, const QUrl &objUrl) {
+			if (!obj && url == objUrl)
+				QCoreApplication::exit(-1);
+		},
+		Qt::QueuedConnection);
+	engine.load(url);
+
+	if (webAppUrl != "")
+		QQmlProperty(engine.rootObjects()[0], "webAppUrl")
+			.write(webAppUrl);
+
+	return app.exec();
 }
